9371048abf0f809568c02277c838376b054e4a3d
eawebmaster20 <eaweb20@gmail.com>
2025-07-09 02:50:25 +0100

    fix

--- models/InvoiceItem.js
+++ models/InvoiceItem.js
@@ -1,45 +1,45 @@
 module.exports = (sequelize, DataTypes) => {
-  const InvoiceItem = sequelize.define('InvoiceItem', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+  const InvoiceItem = sequelize.define(
+    "InvoiceItem",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      invoice_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "invoices",
+          key: "id",
+        },
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      unit_price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      total: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
     },
-    invoice_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'invoices',
-        key: 'id'
-      }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    unit_price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    },
-    total: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
+    {
+      tableName: "invoice_items",
+      timestamps: false,
     }
-  }, {
-    tableName: 'invoice_items',
-    timestamps: false
-  });
+  );
 
-  InvoiceItem.associate = function(models) {
-    InvoiceItem.belongsTo(models.Invoice, { foreignKey: 'invoice_id' });
-  };
-
-  return InvoiceItem;
-};
+  InvoiceItem.associate = function (models) {
+    InvoiceItem.belongsTo(models.Invoice, { foreignKey: "invoice_id" });
   };
 
   return InvoiceItem;
